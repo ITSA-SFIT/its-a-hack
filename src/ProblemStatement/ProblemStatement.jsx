e46d195f6fee89a9d4bdbdcc4e1b19cff31eff83
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-09-23 01:19:37 +0530

    Timer and Problem statement

--- src/ProblemStatement/ProblemStatement.jsx
+++ src/ProblemStatement/ProblemStatement.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionTitle from "../SectionTitle";
+import title from "../title.svg"
 
 const ProblemStatementCard = () => {
   return (
@@ -17,12 +18,12 @@ const ProblemStatement = () => {
       className="bg-gradient-to-r from-[#2b0303] to-[#000000] text-white lg:px-28 sm:px-20 px-6 py-8 font-inter"
     >
       <SectionTitle>Problem Statement</SectionTitle>
-      <div className="flex relative justify-around mt-10 gap-8 text-center items-baseline select-none flex-wrap">
+      <div className="flex relative justify-around mt-10 gap-8 text-center items-baseline flex-wrap">
         <ProblemStatementCard />
         <ProblemStatementCard />
         <ProblemStatementCard />
-        <h3 className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-2xl w-full font-semibold text-gray-200 shadow-lg">
-          Problem Statement will be provided on the day of the event
+        <h3 className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-2xl w-full font-semibold text-gray-200 shadow-lg flex items-center gap-2 justify-center flex-wrap">
+          Problem Statement will be provided 24 hours before<img src={title} alt="" className="h-[1.6rem] inline"/>starts
         </h3>
       </div>
     </div>
