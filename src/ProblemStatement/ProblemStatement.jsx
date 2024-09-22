5419c17c4636700ed747b27ee93d1c2c324160b8
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2024-09-23 01:09:34 +0530

    Problem Statement

--- src/ProblemStatement/ProblemStatement.jsx
+++ src/ProblemStatement/ProblemStatement.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import SectionTitle from '../SectionTitle'
+import React from "react";
+import SectionTitle from "../SectionTitle";
+
+const ProblemStatementCard = () => {
+  return (
+    <div className="bg-red-950 flex-col p-4 aspect-square blur-sm inline-block m-2 w-64">
+      Don't inspect the code, it's just a placeholder. The actual problem
+      statement will be provided on the day of the event.
+    </div>
+  );
+};
 
 const ProblemStatement = () => {
   return (
-    <div id="ProblemStatement" className="bg-gradient-to-r from-[#2b0303] to-[#000000] text-white lg:px-28 sm:px-20 px-6 py-8 font-inter">
-    <SectionTitle>Problem Statement</SectionTitle>
-    <div className="flex justify-around mt-10 flex-wrap text-center items-baseline">
-        <h1 className='font-bold capitalize text-4xl text-gray-400'>To be Released on 26th September</h1>
+    <div
+      id="ProblemStatement"
+      className="bg-gradient-to-r from-[#2b0303] to-[#000000] text-white lg:px-28 sm:px-20 px-6 py-8 font-inter"
+    >
+      <SectionTitle>Problem Statement</SectionTitle>
+      <div className="flex relative justify-around mt-10 gap-8 text-center items-baseline select-none flex-wrap">
+        <ProblemStatementCard />
+        <ProblemStatementCard />
+        <ProblemStatementCard />
+        <h3 className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] text-2xl w-full font-semibold text-gray-200 shadow-lg">
+          Problem Statement will be provided on the day of the event
+        </h3>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default ProblemStatement
+export default ProblemStatement;
